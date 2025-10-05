b12ffa0f732bac78d9fe4ff87377af6c0f98718c
sai prasan <kit27.ad42@gmail.com>
2025-10-05 09:00:00 +0530

    Add User entity with validation and timestamps

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,7 +1,7 @@
 package main.java.com.ecommerce;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; //Annotation
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Annotation
 
 @SpringBootApplication
 public class EcommerceApplication {
