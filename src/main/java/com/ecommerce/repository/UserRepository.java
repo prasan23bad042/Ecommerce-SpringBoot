f498f2029788ee123bca2aed5a2725babd98a0ab
sai prasan <kit27.ad42@gmail.com>
2025-11-01 09:00:00 +0530

    Add UserRepository interface with query methods

---
+++ src/main/java/com/ecommerce/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+    boolean existByUserName(String username);
+    boolean existByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.username = :identifier OR u.email = :identifier")
+    Optional<User> findByUsernameOrEmail(@Param("identifier") String email);
+}
